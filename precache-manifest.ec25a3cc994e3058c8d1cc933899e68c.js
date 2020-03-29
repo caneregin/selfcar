5f71007e86f57d4c28dd85ccf5fa1918181f135d
Caner Egin <cnrgn96@gmail.com>
2020-03-29 11:05:48 +0300

    Updates

---
+++ precache-manifest.ec25a3cc994e3058c8d1cc933899e68c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8f9b56f87be8f4f122c36e7abf94913",
+    "url": "/selfcar/index.html"
+  },
+  {
+    "revision": "e4fe99e1e23771352575",
+    "url": "/selfcar/static/css/main.40f116a2.chunk.css"
+  },
+  {
+    "revision": "5a902338a947fe1aea92",
+    "url": "/selfcar/static/js/2.226ce09a.chunk.js"
+  },
+  {
+    "revision": "ae7f4b79e54473c4b492e8c087e1778a",
+    "url": "/selfcar/static/js/2.226ce09a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4fe99e1e23771352575",
+    "url": "/selfcar/static/js/main.94e170c1.chunk.js"
+  },
+  {
+    "revision": "052488f95e481bf08a26",
+    "url": "/selfcar/static/js/runtime-main.edbf35af.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/selfcar/static/media/logo.5d5d9eef.svg"
+  }
+]);
